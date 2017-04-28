5c49191a684c16a60f925605e050973633440db8
gerCasas <gcasasg4@gmail.com>
2017-04-28 16:58:18 -0700

    Show Hour Selector without UTC conversion

--- src/components/EventOptionsComponents/ShowHourSelector/ShowHourSelector.js
+++ src/components/EventOptionsComponents/ShowHourSelector/ShowHourSelector.js
@@ -16,8 +16,7 @@ class ShowHourSelector extends Component {
     if (props.showDateHours != null){
 
       let index;
-      for (index = 0; index < props.showDateHours.length; ++index) {
-        let dateMoment = new Date(props.showDateHours[index].date_hour);
+      for (index = 0; index < props.showDateHours.length; ++index) {        
         let dateSplited = props.showDateHours[index].date_hour.toString().split("T");
         this.hourFormated = dateSplited[1];
 
@@ -29,7 +28,7 @@ class ShowHourSelector extends Component {
           <div className="btn-group" role="group" aria-label="...">
             <Link to={`/eventos/`+props.event_id+`/`+props.event_title+`/funciones/`+props.show_date+"/"+this.hourFormated}>
               <button type="button" className={(props.hour_date === this.hourFormated) ? 'active btn btn-default btn-event-option-hour-selector' : 'btn btn-default btn-event-option-hour-selector'}>
-                <span className="hour-event-show-caption">{moment(dateMoment).format("LT")}</span>
+                <span className="hour-event-show-caption">{moment(props.showDateHours[index].date_hour).format("LT")}</span>
               </button>
             </Link>
           </div>
